0aed987179078529792a63d39b2fa90594163b30
David Putnam <davidtputnam@hotmail.com>
2017-02-15 08:51:23 -0600

    Convert Integer to Long

--- Assignment_2/src/Assignment_2/MaintenanceIntervals.java
+++ Assignment_2/src/Assignment_2/MaintenanceIntervals.java
@@ -1,30 +1,45 @@
 package Assignment_2;
 
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MaintenanceIntervals {
-    private final Map<String, Integer> maintenanceIntervals;
+    private final Map<String, Long> maintenanceIntervals;
 
     public MaintenanceIntervals() {
-        maintenanceIntervals = new HashMap<String, Integer>();
-        maintenanceIntervals.put("Oil Change", 6000);
-        maintenanceIntervals.put("Tire Rotation", 8000);
-        maintenanceIntervals.put("Tire Condition Check", 5000);
-        maintenanceIntervals.put("Tire Replacement", 41000);
-        maintenanceIntervals.put("Air Filter Replacement", 14000);
-        maintenanceIntervals.put("Spark Plug Replacement", 32000);
-        maintenanceIntervals.put("Timing Belt Replacement", 65000);
-        maintenanceIntervals.put("Cabin Air Filter Replacement", 22000);
+        maintenanceIntervals = new HashMap<String, Long>();
+        maintenanceIntervals.put("Oil Change", 6000l);
+        maintenanceIntervals.put("Tire Rotation", 8000l);
+        maintenanceIntervals.put("Tire Condition Check", 5000l);
+        maintenanceIntervals.put("Tire Replacement", 41000l);
+        maintenanceIntervals.put("Air Filter Replacement", 14000l);
+        maintenanceIntervals.put("Spark Plug Replacement", 32000l);
+        maintenanceIntervals.put("Timing Belt Replacement", 65000l);
+        maintenanceIntervals.put("Cabin Air Filter Replacement", 22000l);
     }
 
     public Set<String> keySet() {
-        return new HashSet<String>(maintenanceIntervals.keySet());
+        return Collections.unmodifiableSet(maintenanceIntervals.keySet());
     }
 
-    public Integer get(String key) {
-        return new Integer(maintenanceIntervals.get(key));
+    public long get(String key) {
+        return maintenanceIntervals.get(key).longValue();
+    }
+
+    public Map<String, Long> selectMaintenanceDue(long mileage, long within) {
+        Map<String, Long> maintenanceDue = new HashMap<String, Long>();
+
+        for (String key : maintenanceIntervals.keySet()) {
+            long intervalMileage = maintenanceIntervals.get(key).longValue();
+            long milesPast = mileage % intervalMileage;
+            long milesUntil = intervalMileage - milesPast;
+            if (milesUntil < within) {
+                maintenanceDue.put(key, new Long(milesUntil));
+            }
+        }
+
+        return maintenanceDue;
     }
 }
